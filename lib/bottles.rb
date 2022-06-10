d84da4ccea78f92f0eabd102463103cc63a28d38
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2022-06-10 18:20:45 +0700

    Why: Pass 2nd test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Bottles
-  def verse(_arg)
+  def verse(number)
     <<~VERSE
-      99 bottles of beer on the wall, 99 bottles of beer.
-      Take one down and pass it around, 98 bottles of beer on the wall.
+      #{number} bottles of beer on the wall, #{number} bottles of beer.
+      Take one down and pass it around, #{number - 1} bottles of beer on the wall.
     VERSE
   end
 end
