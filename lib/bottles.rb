197019f23b679918e36812301f233249b02c1576
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2022-06-10 18:26:42 +0700

    Why: Pass the 3rd test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -4,7 +4,12 @@ class Bottles
   def verse(number)
     <<~VERSE
       #{number} bottles of beer on the wall, #{number} bottles of beer.
-      Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+      Take one down and pass it around, #{decrease_bottle(number)} of beer on the wall.
     VERSE
+  end
+
+  def decrease_bottle(number)
+    temp = number - 1
+    temp > 1 ? "#{temp} bottles" : "#{temp} bottle"
   end
 end
